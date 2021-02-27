d8cb6a0c93802abdb98f85d065b328c65ab5a20d
fidelatrstyni <fidelatrstyni@gmail.com>
2021-02-27 23:10:50 +0700

    add Minggu_2

---
+++ MINGGU_2/Minggu2Percobaan3/Assets/Script/ArrangeActions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ArrangeActions : MonoBehaviour
+{
+    private RectTransform panelRectTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void MoveDownOne()
+    {
+        print("(before change)" + gameObject.name + "sibling index =" + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex = panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(currentSiblingIndex - 1);
+        print("(after change)" + gameObject.name + "sibling index =" + panelRectTransform.GetSiblingIndex());
+    }
+
+    public void MoveUpOne()
+    {
+        print("(before change)" + gameObject.name + "sibling index =" + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex = panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(currentSiblingIndex + 1);
+        print("(after change)" + gameObject.name + "sibling index =" + panelRectTransform.GetSiblingIndex());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
