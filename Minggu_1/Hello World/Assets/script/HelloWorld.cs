af901abf6bbce9dbc54f2426b97204218f1e9eb5
fidelatrstyni <fidelatrstyni@gmail.com>
2021-02-27 23:04:30 +0700

    ADD Minggu_1

---
+++ Minggu_1/Hello World/Assets/script/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public string myText;
+    void Start()
+    {
+        //Debug.Log("Hello World");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Hello World");
+        //Debug.Log("myTest");
+    }
+    void OnGUI()
+    {
+        GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+        GUILayout.Label(myText);
+        GUILayout.EndArea();
+    }
+}
