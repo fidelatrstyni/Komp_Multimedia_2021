cbdb41bcaa5787358dbf5c7f342c95f12456c693
fidelatrstyni <fidelatrstyni@gmail.com>
2021-02-27 23:13:43 +0700

    add Minggu_2

---
+++ MINGGU_2/Minggu2Percobaan6/Assets/DisplayChangedTextContent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayChangedTextContent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private InputField InputField;
+    void Start()
+    {
+        InputField = GetComponent<InputField>();
+    }
+
+    // Update is called once per frame
+    public void PrintNewValue()
+    {
+        string msg = "new content = '" + InputField.text + "'";
+        print(msg);
+    }
+}
---
+++ MINGGU_2/Minggu2Percobaan7/Assets/ToggleChangeManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToggleChangeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Toggle toggle;
+    void Start()
+    {
+        toggle = GetComponent<Toggle>();
+    }
+
+    // Update is called once per frame
+    public void PrintNewToggleValue()
+    {
+        bool status = toggle.isOn;
+        print("toggle status = " + status);
+    }
+}
