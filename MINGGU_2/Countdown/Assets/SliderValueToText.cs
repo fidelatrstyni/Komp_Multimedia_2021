41e195a1d9c919b3358cc3f54601e73107e98775
fidelatrstyni <fidelatrstyni@gmail.com>
2021-02-27 23:08:15 +0700

    Add Countdown

---
+++ MINGGU_2/Countdown/Assets/SliderValueToText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider sliderUI;
+    public Text textSliderValue;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    // Update is called once per frame
+    public void ShowSliderValue()
+    {
+        string sliderMessage = "Slider value =" + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
