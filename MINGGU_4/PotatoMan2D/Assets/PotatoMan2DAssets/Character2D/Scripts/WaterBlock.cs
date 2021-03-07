a8ad952535a5101e82329d944a27c9fbc9d54977
fidelatrstyni <fidelatrstyni@gmail.com>
2021-03-07 10:09:16 +0700

    ADD MINGGU 4

---
+++ MINGGU_4/PotatoMan2D/Assets/PotatoMan2DAssets/Character2D/Scripts/WaterBlock.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterBlock : MonoBehaviour
+{
+    private Animator animatorController;
+    void Start()
+    {
+        animatorController = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D hit)
+    {
+        if(hit.CompareTag("Player")){
+            animatorController.SetTrigger("Fall");
+        }
+    }
+}
