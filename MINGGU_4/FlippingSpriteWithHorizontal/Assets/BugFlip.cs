ebd6acb5a5bea142f188bcc3f8a2c11e916ad33c
fidelatrstyni <fidelatrstyni@gmail.com>
2021-03-07 10:02:24 +0700

    ADD MINGGU_4

---
+++ MINGGU_4/FlippingSpriteWithHorizontal/Assets/BugFlip.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugFlip : MonoBehaviour
+{
+    private bool facingRight = true;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && facingRight)
+            Flip();
+        if(Input.GetKeyDown(KeyCode.RightArrow) && !facingRight)
+            Flip();
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
